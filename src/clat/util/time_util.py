6ca04f7d4c1d3c99e1e805f1e16b1e0d771132f1
Allen Chen <allenmuhanchen@gmail.com>
2024-01-08 10:14:03 -0500

    added more time_util methods for specific date and time ranges

--- src/clat/util/time_util.py
+++ src/clat/util/time_util.py
@@ -42,3 +42,56 @@ def now():
 
 def to_unix(dt: datetime.datetime):
     return round(time.mktime(dt.timetuple()) * 1000000)
+
+
+def on_date(year: int, month: int, day: int) -> When:
+    """
+    Returns a When object representing the start and end of the 24-hour period for the given date.
+
+    :param year: Year component of the date.
+    :param month: Month component of the date.
+    :param day: Day component of the date.
+    :return: When object representing the start and end of the given date.
+    """
+    date = datetime.date(year, month, day)
+    start = to_unix(date)
+    end = to_unix(date + datetime.timedelta(days=1))
+    return When(start, end)
+
+
+def on_date_and_time(year: int, month: int, day: int, start_time: str = None, end_time: str = None) -> When:
+    """
+    Returns a When object representing the period from the given start date and optional start time to an optional end time or the end of the day if no end time is provided.
+
+    :param year: Year component of the date.
+    :param month: Month component of the date.
+    :param day: Day component of the date.
+    :param start_time: Optional start time as a string in 'HH:MM:SS' format. Defaults to the beginning of the day if not provided.
+    :param end_time: Optional end time as a string in 'HH:MM:SS' format. Defaults to the end of the day if not provided.
+    :return: When object representing the start and potential end of the given date and time.
+    """
+    # Determine the start time
+    if start_time:
+        # Create the start datetime from the date and time components
+        start_datetime = datetime.datetime(year, month, day) + datetime.timedelta(seconds=int(start_time[:2])*3600 + int(start_time[3:5])*60 + int(start_time[6:]))
+    else:
+        # Default to the beginning of the day (00:00:00)
+        start_datetime = datetime.datetime(year, month, day)
+    start = to_unix(start_datetime)
+
+    # Determine the end time
+    if end_time:
+        # Create the end datetime from the date and time components
+        end_datetime = datetime.datetime(year, month, day) + datetime.timedelta(seconds=int(end_time[:2])*3600 + int(end_time[3:5])*60 + int(end_time[6:]))
+    else:
+        # Default to the end of the day (23:59:59)
+        end_datetime = datetime.datetime(year, month, day, 23, 59, 59)
+    end = to_unix(end_datetime)
+
+    return When(start, end)
+
+
+# Testing the new function with a start time and default end time
+test_start_time = "08:30:00"  # 8:30 AM
+when_for_date_time = on_date_and_time(2022, 7, 4, test_start_time)
+when_for_date_time
